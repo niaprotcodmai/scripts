e9e134a1cbaa98df12d54eeea836423ce3fe70d0
niaprotcodmai <gregkuhn420@gmail.com>
2022-09-26 14:03:31 -0500

    ver1 completed and tested

--- phx/remove-mock_transactions.ps1
+++ phx/remove-mock_transactions.ps1
@@ -5,9 +5,12 @@ $folder = 'C:\data-test'
 
 if (Test-Path -Path $folder) {
     Write-Host "Mock data exists. Continuing..."
-    }
+    
+    #delete all mock data
+    Remove-Item 'C:\data-test\2382\*' -Recurse
 
-
+    Write-Host "All mock data was deleted!"
+}
  else {
      Write-Host "This PC does not contain any mock data."
      exit 1
